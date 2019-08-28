e25672b29def9e9e06ba6c07adb13c5d38e9ff9a
Noah Thorp <noah@comakery.com>
2019-08-28 20:27:38 +0200

    test constructor setup

--- contracts/contracts/ERC1404.sol
+++ contracts/contracts/ERC1404.sol
@@ -1,6 +1,5 @@
 pragma solidity ^ 0.5 .0;
 
-
 contract ERC1404 {
   string public symbol;
   string public name;
@@ -14,21 +13,26 @@ contract ERC1404 {
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 
-
-  constructor(address _initialTokenHolder, string memory _symbol) public {
-    require(_initialTokenHolder != address(0), "Token holder address cannot be 0x0");
-    
-    symbol = _symbol;
-    name = "foo";
-    decimals = 18;
-    totalSupply = 1e27;
-
-    _balances[_initialTokenHolder] = totalSupply;
-  }
-
   modifier validAddress(address _address) {
     require(_address != address(0), "Error 0x0 is an invalid address");
     _;
+  }
+
+  constructor(
+    address _initialTokenHolder, 
+    string memory _symbol,
+    string memory _name,
+    uint8 _decimals,
+    uint256 _totalSupply
+  ) public {
+    require(_initialTokenHolder != address(0), "Token holder address cannot be 0x0");
+
+    symbol = _symbol;
+    name = _name;
+    decimals = _decimals;
+    totalSupply = _totalSupply;
+
+    _balances[_initialTokenHolder] = totalSupply;
   }
 
   function balanceOf(address owner) public view returns(uint256 balance) {
@@ -40,17 +44,16 @@ contract ERC1404 {
   }
 
   function transfer(address to, uint256 value) public returns(bool success) {
-    privateTransfer(msg.sender, to, value);
+    _transfer(msg.sender, to, value);
     return true;
   }
 
-  /**
-   * IT IS RECOMMENDED THAT YOU USE THE safeApprove() FUNCTION INSTEAD OF approve() TO AVOID A TIMING ISSUES WITH THE ERC20 STANDARD.
-   * The approve function implements the standard to maintain backwards compatibility with ERC20.
-   * Read more about the race condition exploit of approve here https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+  /*  IT IS RECOMMENDED THAT YOU USE THE safeApprove() FUNCTION INSTEAD OF approve() TO AVOID A TIMING ISSUES WITH THE ERC20 STANDARD.
+      The approve function implements the standard to maintain backwards compatibility with ERC20.
+      Read more about the race condition exploit of approve here https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
    */
   function approve(address spender, uint256 value) public validAddress(spender) returns(bool success) {
-    return privateApprove(spender, value);
+    return _approve(spender, value);
   }
 
   // Use safeApprove() instead of approve() to avoid the race condition exploit which is a known security hole in the ERC20 standard
@@ -59,7 +62,7 @@ contract ERC1404 {
   returns(bool success) {
     require(expectedApprovedValue == _allowed[msg.sender][spender], "The expected approved amount does not match the actual approved amount");
     require(nonce == _approvalNonces[msg.sender][spender], "The nonce does not match the current transfer approval nonce");
-    return privateApprove(spender, newApprovalValue);
+    return _approve(spender, newApprovalValue);
   }
 
   // fetch the current allowed transfers for a sender and receiver along with the spender's nonce
@@ -69,30 +72,31 @@ contract ERC1404 {
     return (_allowance, _nonce);
   }
 
-  // use a nonce to enfore expected approval amounts for the approve and safeApprove functions
-  function privateApprove(address spender, uint256 value) internal returns(bool success) {
-    // intentional allowance for an overflow
-    _approvalNonces[msg.sender][spender]++;
+  /********** INTERNAL FUNCTIONS **********/
+  function transferFrom(address from, address to, uint256 value) public returns(bool success) {
+    require(value <= _allowed[from][to], "The approved allowance is lower than the transfer amount");
+    _allowed[from][msg.sender] = sub(_allowed[from][msg.sender], value);
+    _transfer(from, to, value);
+    return true;
+  }
+
+  function _approve(address spender, uint256 value) internal returns(bool success) {
+    // use a nonce to enforce expected approval amounts for the approve and safeApprove functions
+    _approvalNonces[msg.sender][spender]++; // intentional allowance for an overflow
     _allowed[msg.sender][spender] = value;
     emit Approval(msg.sender, spender, value);
     return true;
   }
 
-  function transferFrom(address from, address to, uint256 value) public returns(bool success) {
-    require(value <= _allowed[from][to], "The approved allowance is lower than the transfer amount");
-    _allowed[from][msg.sender] = sub(_allowed[from][msg.sender], value);
-    privateTransfer(from, to, value);
-    return true;
-  }
-
-  function privateTransfer(address from, address to, uint256 value) internal validAddress(to) {
+  function _transfer(address from, address to, uint256 value) internal validAddress(to) {
     require(value <= _balances[to], "Insufficent tokens");
-    require(to != address(this), "To address cannot be this contract");
+    require(to != address(this), "Error: cannot transfer tokens to the token contract address - the tokens would be permanently unrecoverable.");
     _balances[from] = sub(_balances[from], value);
     _balances[to] = add(_balances[to], value);
     emit Transfer(from, to, value);
   }
 
+  /********** SAFE MATH **********/
   function sub(uint256 a, uint256 b) internal pure returns(uint256 result) {
     require(b <= a, "Underflow error");
     uint256 c = a - b;
--- contracts/test/ERC1404Test.sol
+++ contracts/test/ERC1404Test.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.8;
+pragma solidity ^ 0.5 .8;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
@@ -6,13 +6,27 @@ import "../contracts/ERC1404.sol";
 
 contract ERC1404Test {
     ERC1404 token;
+    address initialTokenHolder;
 
     function beforeEach() public {
-        token = new ERC1404(address(this), "xyz");
+        initialTokenHolder = address(this);
+        token = new ERC1404(initialTokenHolder, "xyz", "Ex Why Zee", 6, 1234567);
     }
 
-    function testSymbolName() public {
-        string memory symbol = 'xyz';
-        Assert.equal(token.symbol(), symbol, "should return the token symbol");
-    }    
+    function testTokenInitialization() public {
+        Assert.equal(token.symbol(), "xyz", "should return the token symbol");
+        Assert.equal(token.name(), "Ex Why Zee", "should return the token name");
+
+        Assert.equal(uint(token.decimals()), 6, "should return the token decimals");
+        Assert.equal(uint(token.totalSupply()), 1234567, "should return the totalSupply");
+
+    }
+
+    function testInitialTokenHolderGetsTotalSupply() public {
+        Assert.isTrue(token.totalSupply() > 1, "there should be tokens issued");
+
+        Assert.equal(uint(token.totalSupply()),
+            token.balanceOf(initialTokenHolder),
+            "all the tokens should be in the initialTokenHolder address");
+    }
 }
