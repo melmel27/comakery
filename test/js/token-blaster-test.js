904277a33a88e804274326a3e363d8486ef85a96
Noah Thorp <noah@comakery.com>
2019-12-23 22:29:06 -0800

    Simple multiTransfer

--- test/js/token-blaster-test.js
+++ test/js/token-blaster-test.js
@@ -4,11 +4,11 @@ var TransferRules = artifacts.require("TransferRules");
 // var blaster = require('token-blaster')
 
 class TokenBlaster {
-    
     constructor(token, tokenAddress, walletAddress) {
         this.token = token
         this.tokenAddress = tokenAddress
         this.walletAddress = walletAddress
+        this.transfer = this.transfer.bind(this)
     }
 
     static async init(tokenAddress, walletAddress) {
@@ -17,18 +17,28 @@ class TokenBlaster {
     }
 
     async transfer(recipientAddress, amount) {
-        return await this.token.transfer(recipientAddress, amount, {from: this.walletAddress})
+        return await this.token.transfer(recipientAddress, amount, {
+            from: this.walletAddress
+        })
+    }
+
+    async multiTransfer(recipientAddressAndAmountArray) {
+        let promises = recipientAddressAndAmountArray.map(([recipientAddress, amount]) => {
+            return this.transfer(recipientAddress, amount)
+        })
+        return Promise.all(promises)
     }
 }
 
 contract("TokenBlaster", function (accounts) {
-    var contractAdmin
+    var sendWallet
     var alice
     var bob
     var token
+    var blaster
 
     beforeEach(async function () {
-        contractAdmin = accounts[0]
+        sendWallet = accounts[0]
         alice = accounts[1]
         bob = accounts[2]
         charlie = accounts[3]
@@ -36,30 +46,55 @@ contract("TokenBlaster", function (accounts) {
         defaultGroup = 0
 
         let rules = await TransferRules.new()
-        token = await RestrictedToken.new(rules.address, contractAdmin, contractAdmin, "xyz", "Ex Why Zee", 6, 100, 1e6)
+        token = await RestrictedToken.new(rules.address, sendWallet, sendWallet, "xyz", "Ex Why Zee", 6, 100, 1e6)
 
-        await token.grantTransferAdmin(contractAdmin, {
-            from: contractAdmin
+        await token.grantTransferAdmin(sendWallet, {
+            from: sendWallet
         })
 
         await token.setAllowGroupTransfer(defaultGroup, defaultGroup, 1, {
-            from: contractAdmin
+            from: sendWallet
         })
 
         await token.setAddressPermissions(bob, defaultGroup, 1, 200, false, {
-            from: contractAdmin
+            from: sendWallet
         })
+
+        blaster = await TokenBlaster.init(token.address, sendWallet)
+
     })
 
     it('can do a simple transfer', async () => {
-        let blaster = await TokenBlaster.init(token.address, contractAdmin)
         let tx = await blaster.transfer(bob, 50)
         assert.equal(await token.balanceOf.call(bob), 50)
 
         truffleAssert.eventEmitted(tx, 'Transfer', (ev) => {
-            assert.equal(ev.from, contractAdmin)
+            assert.equal(ev.from, sendWallet)
             assert.equal(ev.to, bob)
             assert.equal(ev.value, 50)
+            return true
+        })
+    })
+
+    it('can do a simple multiTransfer', async () => {
+        let txns = await blaster.multiTransfer([
+            [bob, 23],
+            [bob, 27]
+        ])
+        assert.equal(await token.balanceOf.call(bob), 50)
+        // var txns = await Promise.all(txnsP)
+
+        truffleAssert.eventEmitted(txns[0], 'Transfer', (ev) => {
+            assert.equal(ev.from, sendWallet)
+            assert.equal(ev.to, bob)
+            assert.equal(ev.value, 23)
+            return true
+        })
+
+        truffleAssert.eventEmitted(txns[1], 'Transfer', (ev) => {
+            assert.equal(ev.from, sendWallet)
+            assert.equal(ev.to, bob)
+            assert.equal(ev.value, 27)
             return true
         })
     })
